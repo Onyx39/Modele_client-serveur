64798b77064653a24a46a7244bebfff1641c5600
Onyx39 <valentin.richard39@sfr.fr>
2022-09-19 11:59:28 +0200

    1er essai mais bon ca ne marche pas...

---
+++ src/bd.sql
@@ -0,0 +1,11 @@
+-- id : richaval
+-- mdp : kwia6s9y
+
+CREATE TABLE IF NOT EXISTS Record (
+    RecordID INT(11) NOT NULL AUTO_INCREMENT, 
+    URL text NOT NULL, 
+    PRIMARY KEY (RecordID)
+)
+
+ENGINE=InnoDB DEFAULT charset=utf8 AUTO_INCREMENT=1;
+
